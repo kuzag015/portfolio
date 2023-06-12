48206c453b5503ed3434a35ad34be7e0b7ec88a3
Gabriel Kuzava <101483110+kuzag015@users.noreply.github.com>
2023-06-12 14:18:50 -0700

    changed hero background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import {ComputersCanvas} from './canvas'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto bg-gradient-to-r from-cyan-600 to-orange-300'>
+    <section className= 'relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#cbb3ff]'/>
